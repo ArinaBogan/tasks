9b7d39505c4bc35b5f5a0d69af80034e96617273
ArinaBogan <arina.bogan@gmail.com>
2023-07-04 19:10:52 +0300

    add str 1-15

--- 013.String/14/app.js
+++ 013.String/14/app.js
@@ -1,2 +1,7 @@
 // На вход подается url. Если он начинается с http, содержит хотя бы один “/” и
 // заканчивается подстрокой .com или .ru, то вывести true, в противном случае false
+
+const a = prompt('введите url');
+
+console.log((a.includes('http') && a.includes('/')) && (a.includes('.com') || a.includes('.ru')) ? true : false);
+
