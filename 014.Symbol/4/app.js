de9777d9b53082fc1dc4168f550a5dc80f3d6d55
ArinaBogan <arina.bogan@gmail.com>
2023-07-06 20:30:19 +0300

    add str

---
+++ 014.Symbol/4/app.js
@@ -0,0 +1,10 @@
+// На вход подаются 2 строки. Необходимо сравнить их и если они одинаковы, то
+// вывести true, в противном случае false.
+// 'string', 'StRiNg' -> true
+
+let a = 'string'.toLowerCase();
+let b = 'StRiNg'.toLowerCase();
+
+console.log(a === b ? true : false);
+
+
