bcf57ca7383bebdc4bef8b8e9376a43fcb293bf5
ArinaBogan <arina.bogan@gmail.com>
2023-07-20 19:30:49 +0300

    added tasks

--- 016.Cycles/20/app.js
+++ 016.Cycles/20/app.js
@@ -11,7 +11,6 @@ for (let i = 0; i < num.length; i++) {
         res.push(num[i]);
         continue;
     }
-    // let last_el = res[res.length - 1];
     if (num[i] % 2 !== 0 && num[i - 1] % 2 !== 0) {
         res.push(':', num[i]);
         continue;
