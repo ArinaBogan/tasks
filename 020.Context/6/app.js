d3b1ca8aaa36c0b4b299c136c58aecb5d7a61538
ArinaBogan <arina.bogan@gmail.com>
2023-08-01 00:42:12 +0300

    added tasks

---
+++ 020.Context/6/app.js
@@ -0,0 +1,18 @@
+// Работа с замыканием. Реализуйте функцию, которая будет считать количество
+// своих вызовов
+
+function countNumber() {
+    let count = 0;
+    return function () {
+        count += 1;
+        console.log(count);
+    }
+}
+const result = countNumber();
+
+result();
+result();
+result();
+result();
+result();
+
