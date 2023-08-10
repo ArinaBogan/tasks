435e7766b6d478c7044bd862a791828b7811d45c
ArinaBogan <arina.bogan@gmail.com>
2023-08-11 00:39:28 +0300

    added tasks

--- 022.Error/9/app.js
+++ 022.Error/9/app.js
@@ -9,3 +9,24 @@
 // мальчик. Гарантируется, что на пути всегда будет хотя бы один нулевой дом.
 // Добавить проверку, что номера домов – только числа.
 // [5, 1, 2, 3, 0, 1, 5, 0, 2] –> 11 (5 + 1 + 2 + 3 = 11)
+
+const arr = [5, 1, 2, 3, 0, 1, 5, 0, 2];
+
+function doSum(arr) {
+    try {
+        let sum = 0;
+        for (let i = 0; i < arr.length; i++) {
+            if (isNaN(arr[i])) throw new Error('введите число');
+            if (arr[i] !== 0) {
+                sum += arr[i];
+            } else {
+                break;
+            }
+        }
+        return sum;
+    } catch (error) {
+        return error.message;
+    }
+}
+const result = doSum(arr);
+console.log(result);
