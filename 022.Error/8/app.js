ff1298e1a4e49ff219f8412d5f8553e0277d6c6d
ArinaBogan <arina.bogan@gmail.com>
2023-08-11 13:26:24 +0300

    added tasks

--- 022.Error/8/app.js
+++ 022.Error/8/app.js
@@ -2,3 +2,24 @@
 // (возрастающих) букв и возвращает отсутствующую в массиве. Добавить проверки
 // ["a","b","c","d","f"] -> "e"
 // ["O","Q","R","S"] -> "P"
+
+const inpStr = ["O", "Q", "R", "S"].join('').toLowerCase();
+
+function findLetter(inpStr) {
+    try {
+        const alfa = 'abcdefghijklmnopqrstuvwxyz';
+        const indexStartInput = alfa.indexOf(inpStr[0]);
+
+        const sliceFromAlfa = alfa.slice(indexStartInput, indexStartInput + inpStr.length + 1);
+
+        for (let i = 0; i < sliceFromAlfa.length; i++) {
+            if (sliceFromAlfa[i] !== inpStr[i]) {
+                console.log(sliceFromAlfa[i]);
+                break;
+            }
+        }
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+findLetter(inpStr)
