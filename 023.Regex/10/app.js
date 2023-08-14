d059fd72b2a64efd65fe4da5058f5ac70af84a87
ArinaBogan <arina.bogan@gmail.com>
2023-08-14 23:55:51 +0300

    added tasks

--- 023.Regex/10/app.js
+++ 023.Regex/10/app.js
@@ -3,3 +3,23 @@
 // Вырезая куски кода вы поняли, что их чересчур много. Необходимо написать
 // регулярное выражение, которое вырежет все комментарии из html разметки
 // <!-- -->
+
+const str = ` <div class="123">
+<ul>
+<!-- asdcf -->
+<li></li>
+<li></li>
+<!--njgkk 123-->
+</ul>
+</div>`;
+
+function deleteComments(str) {
+    try {
+        let newStr = str.replaceAll(/<!--[\w\.\*\(\)-_! ]+-->/g, '');
+        return newStr;
+    } catch (error) {
+        return error.message;
+    }
+}
+const result = deleteComments(str);
+console.log(result);
