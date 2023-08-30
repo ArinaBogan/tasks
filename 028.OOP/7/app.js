7b05cf905da912c1a4138700f7936950b1a3e6ca
ArinaBogan <arina.bogan@gmail.com>
2023-08-30 23:11:49 +0300

    added tasks

--- 028.OOP/7/app.js
+++ 028.OOP/7/app.js
@@ -15,3 +15,38 @@
 // }`
 // Необходимо вывести в консоль найденный элемент массива по id если таковой
 // имеется. В противном случае бросить исключение. Добавить проверки 
+
+class ServerById {
+    controller(obj) {
+        try {
+            const serv = this.service(obj);
+            return serv;
+        } catch (error) {
+            return error.message;
+        }
+    }
+
+    service(obj) {
+        const rep = this.repository(obj);
+        return rep;
+    }
+
+    repository(obj) {
+        const arr = [
+            { "id": "javascript", "label": "JavaScript", "category": "programmingLanguages", "priority": 1 },
+            { "id": "typescript", "label": "TypeScript", "category": "programmingLanguages", "priority": 1 },
+            { "id": "sql", "label": "SQL", "category": "programmingLanguages", "priority": 2 },
+            { "id": "java", "label": "Java", "category": "programmingLanguages", "priority": 3 },
+            { "id": "go", "label": "GO", "category": "programmingLanguages", "priority": 3 }
+        ]
+        const result = arr.filter((el) => el.id == obj.id ? true : null)
+        return result
+    }
+}
+const serverById = new ServerById();
+
+const obj = {
+    id: 'javascript'
+}
+
+console.log(serverById.controller(obj));
