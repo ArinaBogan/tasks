62be174f0726737170128dbcc933490542cd424a
ArinaBogan <arina.bogan@gmail.com>
2023-09-05 19:28:57 +0300

    added tasks

--- 029.SOLID/12/app.js
+++ 029.SOLID/12/app.js
@@ -1,2 +1,18 @@
 // Реализуйте класс Sort, сдержащий метод sortData, сортирующий все числа
 // статичного массива сортировкой пузырьком
+class Sort {
+    sortData(arr) {
+        for (let j = arr.length - 1; j > 0; j--) {
+            for (let i = 0; i < j; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    let temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                }
+            }
+        }
+        console.log(arr);
+    }
+}
+const sort = new Sort();
+sort.sortData([1, 5, 4, 3, 2])
--- 030.Extends/4/app.js
+++ 030.Extends/4/app.js
@@ -4,3 +4,49 @@
 // рассчитываться по математическим формулам. Класс Figure содержит поля: pi =
 // 3.14, r = radius, a = side, h = height, а также сеттеры для данных полей. Дочерние
 // классы содержать геттеры на получение площади
+class Figure {
+    pi = 3.14;
+    r;
+    a;
+    h;
+    name;
+    setRadius(r) {
+        this.r = r;
+    };
+    setSide(a) {
+        this.a = a;
+    };
+    setHeight(h) {
+        this.h = h;
+    };
+}
+class Circle extends Figure {
+    name = 'Площадь круга равна';
+    getSofCircle() {
+        return `${this.name} ${this.pi * this.r ** 2}`
+    }
+}
+class Triangle extends Figure {
+    name = 'Площадь треугольника равна';
+    getSofTriangle() {
+        return `${this.name} ${1 / 2 * this.h * this.a}`
+    }
+}
+class Square extends Figure {
+    name = 'Площадь квадрата равна';
+    getSofSquare() {
+        return `${this.name} ${this.a ** 2}`
+    }
+}
+const circle = new Circle();
+circle.setRadius(3);
+console.log(circle.getSofCircle());
+
+const triangle = new Triangle();
+triangle.setHeight(4);
+triangle.setSide(4);
+console.log(triangle.getSofTriangle());
+
+const square = new Square();
+square.setSide(5);
+console.log(square.getSofSquare());
