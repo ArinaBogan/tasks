6dfe51c1e4c85401e79d88c2e60e0c2fa891608c
ArinaBogan <arina.bogan@gmail.com>
2023-09-19 22:21:51 +0300

    add tasks

--- 020.Context/10/app.js
+++ 020.Context/10/app.js
@@ -2,24 +2,24 @@
 // нескольких слов и букву. Функция должна подсчитывать количество вхождений
 // указанной буквы в строке. Добавить проверки
 
-const letter = prompt('введите букву');
-const string = prompt('введите предложение');
-
-
-
-function doSum(l, str) {
-    let count = 0;
-    if (isNaN(l) && isNaN(str)) {
-        for (let i = 0; i < str.length; i++) {
-            if (str[i] == l) {
-                count++
+class DoCount {
+    isValid(l, str) {
+        if (!isNaN(l) || !isNaN(str)) throw new Error('некорректное значение');
+    }
+    doSum(l, str) {
+        try {
+            this.isValid(l,str);
+            let count = 0;
+            for (let i = 0; i < str.length; i++) {
+                if (str[i] == l)
+                    count++
             }
+            console.log(count);
+        } catch (error) {
+           console.log(error.message);
         }
-        return count;
-    } else {
-        console.log('ошибка ввода');
+
     }
 }
-
-const result = doSum(letter, string);
-console.log(result);
+const doCount = new DoCount();
+doCount.doSum('a','arina');
