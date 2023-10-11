9e4cacb66b737a6bd5c8bc388e25d2eb678971d8
ArinaBogan <arina.bogan@gmail.com>
2023-10-11 20:00:39 +0300

    added tasks

--- 034.Repeat_2/10/app.js
+++ 034.Repeat_2/10/app.js
@@ -4,17 +4,32 @@ const btnBack = document.querySelector('.back');
 const btnPlay = document.querySelector('.play');
 const btnNext = document.querySelector('.next');
 const audio = document.querySelector('audio');
+const artist = document.querySelector('.artist');
+const nameOfSong = document.querySelector('.nameOfSong');
+const songCover = document.querySelector('.songCover');
 
 let flag = false;
 const songs = [
-    { id: 1, path: './songs/song.mp3' },
-    { id: 2, path: './songs/song1.mp3' },
-    { id: 3, path: './songs/song2.mp3' }
+    {
+        id: 1, path: './songs/song.mp3', artist: 'artist', nameOfSong: 'song',
+        songCover: 'background-image:url(./assets/img1.jpg)'
+    },
+    {
+        id: 2, path: './songs/song1.mp3', artist: 'artist1', nameOfSong: 'song1',
+        songCover: 'background-image:url(./assets/img2.jpg)'
+    },
+    {
+        id: 3, path: './songs/song2.mp3', artist: 'artist2', nameOfSong: 'song2',
+        songCover: 'background-image:url(./assets/img3.jpg)'
+    }
 ];
 let currentIndexSong = 0;
 
 btnPlay.addEventListener('click', function () {
     audio.src = songs[currentIndexSong].path;
+    artist.textContent = songs[currentIndexSong].artist;
+    nameOfSong.textContent = songs[currentIndexSong].nameOfSong;
+    songCover.style = songs[currentIndexSong].songCover;
     if (flag == false) {
         audio.play();
         this.style = 'background-image:url(./assets/pause.svg)';
@@ -29,7 +44,10 @@ btnPlay.addEventListener('click', function () {
 btnBack.addEventListener('click', () => {
     if (currentIndexSong == 0) return;
     currentIndexSong--;
+    artist.textContent = songs[currentIndexSong].artist;
+    nameOfSong.textContent = songs[currentIndexSong].nameOfSong;
     audio.src = songs[currentIndexSong].path;
+    songCover.style = songs[currentIndexSong].songCover;
     audio.play();
     flag = true;
     btnPlay.style = 'background-image:url(./assets/pause.svg)';
@@ -38,6 +56,9 @@ btnBack.addEventListener('click', () => {
 btnNext.addEventListener('click', () => {
     if (currentIndexSong == songs.length - 1) return
     currentIndexSong++;
+    artist.textContent = songs[currentIndexSong].artist;
+    nameOfSong.textContent = songs[currentIndexSong].nameOfSong;
+    songCover.style = songs[currentIndexSong].songCover;
     audio.src = songs[currentIndexSong].path;
     audio.play();
     flag = true;
